ffac340834260b1329779bec5c026e534ff6df4c
7ronbon <7ronbon@gmail.com>
2024-08-04 00:23:24 -0400

    small changes

--- app/page.js
+++ app/page.js
@@ -31,10 +31,114 @@ const theme = createTheme( {
 }); 
  
 function App() {
+  //some const functions as needed
+  const [ items, setItems ] = useState([]);
+  
+  const [ newItem, setNewItem ] = useState( {name: '', amt:'', cost:''})
+  const [ total, setTotal ] = useState(0)
+  const [searchQuery, setsearchQuery] = useState('');
+
+  // add items to database
+  const addItem = async (e) => {
+    e.preventDefault();
+    if (newItem.name !== '' && newItem.amt !== '' && newItem.cost !== ''){
+      setItems([...items, newItem]);
+      await addDoc(collection(db, 'items'), { name: newItem.name.trim() , amt: newItem.amt.trim() , cost: newItem.cost.trim() });
+      setNewItem({name: '', amt:'', cost: ''});
+    }
+  }
+
+  // read items from database
+  useEffect(() => {
+    const q = query(collection(db, 'items'));
+    const unsubscribe = onSnapshot( q, (QuerySnapshot) => {
+      let itemsArr = [];
+
+      QuerySnapshot.forEach((doc)=>{
+        itemsArr.push({...doc.data(), id: doc.id});
+      })
+      setItems(itemsArr);
+
+      // read total from itemsArr
+      const caluTotal = () => {
+        const totalPrice = itemsArr.reduce((sum,item) => sum + parseFloat(item.cost * item.amt), 0);
+        setTotal(totalPrice);
+      };
+      caluTotal();
+      return() => unsubscribe();
+    });
+  },[]);
+
+  // delete items from database
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, 'items', id));
+  }
+
+  //search bar
+
+  //edit item
+
+  //return
   return (
-  <Container sx = {{ bgcolor:"pink", textDecorationColor: 'blue'}}>
-    <Box display="flex">
-      <typography varient="h1">BLAH</typography>
+  <Container sx = {{ bgcolor:"black", textDecorationColor: 'blue'}}>
+    <Box display="flex" sx={{align:"center"}}>
+    
+    {/* start interior */}
+    <main className="flex min-h-screen items-center justify-between">
+      <Box className="z-10 w-1000 items-center justify-between font-mono text-sm lg:flex">
+
+      {/* start form */}
+      {/* https://tailwindcss.com/docs/background-color */}
+      <Box className="bg-orange-900 p-3 items-center">
+        <h1 className="text-3xl p-3 text-center">the pantry...</h1>
+        <form className="grid-col-12 items-center text-black">
+          <input value={newItem.name} onChange={(e) => setNewItem({...newItem, name: e.target.value}) } className="col-3 p-3 border" type="text" placeholder="item name?"/>
+          <input value={newItem.amt} onChange={(e) => setNewItem({...newItem, amt: e.target.value}) } className="col-3 p-3 border" type="number" placeholder="amt of items?"/>
+          <input value={newItem.cost} onChange={(e) => setNewItem({...newItem, cost: e.target.value}) } className="col-3 p-3 border" type="number" placeholder="cost per item?"/>
+          <button onClick={addItem} className="col-3 p-3 bg-orange-800 hover:bg-orange-600 p-3 text-white" type="submit">add</button>
+        </form>
+
+        {/* search and edit button below */}
+        {items.map(() => (''))}
+        {items.length !== 0 ? (
+          <Box>
+            <h1 className="text-3xl p-3 text-center">current items...</h1>
+            <Box className="flex justify-between p-3">
+              <span><input className="col-span-3 p-2 border text-black" type="text" placeholder="search items..."/></span>
+              <span><button onClick={() => editItem(item)} className="p-3 bg-orange-800 hover:bg-orange-600">edit mode</button></span>
+            </Box>  
+          </Box>
+        ):('')}
+      
+        <ul>
+          {items.map((item, id) => (
+            <li key={id} className="my-4 w-full flex justify-between mx-0 bg-orange-800">
+              <div className="p-3 w-full flex justify-between mx-3"> 
+                <span className="lowercase">{item.name}</span>
+                <span>x{item.amt}</span>
+                <span>${item.cost}</span>
+              </div>
+              <button onClick={() => deleteItem(item.id)} className="p-3 border-orange-900 hover:bg-orange-600 w-10">x</button>
+            </li>
+          ))}
+        </ul>
+
+        {/* only show the item list IF ONLY there are items */}
+        {items.length < 1 ? ('there are no items in your bag...') : ( 
+          <Box>
+            <Box className="flex justify-between p-3">
+              <span>total :</span> <span>${total}</span>
+            </Box>
+          </Box>
+        )}
+
+      </Box>
+      {/* end form */}
+
+      </Box>
+    </main>
+    {/* end interior */}
+
     </Box>
   </Container>
   )
@@ -46,8 +150,7 @@ export default App;
 
 
 // export default function Home() {
-//   const [ items, setItems ] = useState([
-//   ]);
+//   const [ items, setItems ] = useState([]);
   
 //   const [ newItem, setNewItem ] = useState( {name: '', amt:'', cost:''})
 //   const [ total, setTotal ] = useState(0)
@@ -89,42 +192,42 @@ export default App;
 //   }
 
 //   return (
-//     <main className="flex min-h-screen items-center justify-between p-24">
-//       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    // <main className="flex min-h-screen items-center justify-between p-24">
+    //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
-//       {/* https://tailwindcss.com/docs/background-color */}
-//       <div className="bg-orange-900 p-3 rounded-lg items-center">
-//         <h1 className="text-3xl p-3 text-center">the pantry...</h1>
-//         <form className="grid grid-col-12 items-center text-black">
-//           <input value={newItem.name} onChange={(e) => setNewItem({...newItem, name: e.target.value}) } className="col-span-3 p-3 border" type="text" placeholder="item name?"/>
-//           <input value={newItem.amt} onChange={(e) => setNewItem({...newItem, amt: e.target.value}) } className="col-span-3 p-3 border" type="number" placeholder="amt of items?"/>
-//           <input value={newItem.cost} onChange={(e) => setNewItem({...newItem, cost: e.target.value}) } className="col-span-3 p-3 border" type="number" placeholder="cost per item?"/>
-//           <button onClick={addItem} className="bg-orange-800 hover:bg-orange-600 p-3 text-xl rounded-lg text-white" type="submit">add</button>
-//         </form>
+    //   {/* https://tailwindcss.com/docs/background-color */}
+    //   <div className="bg-orange-900 p-3 rounded-lg items-center">
+    //     <h1 className="text-3xl p-3 text-center">the pantry...</h1>
+    //     <form className="grid grid-col-12 items-center text-black">
+    //       <input value={newItem.name} onChange={(e) => setNewItem({...newItem, name: e.target.value}) } className="col-span-3 p-3 border" type="text" placeholder="item name?"/>
+    //       <input value={newItem.amt} onChange={(e) => setNewItem({...newItem, amt: e.target.value}) } className="col-span-3 p-3 border" type="number" placeholder="amt of items?"/>
+    //       <input value={newItem.cost} onChange={(e) => setNewItem({...newItem, cost: e.target.value}) } className="col-span-3 p-3 border" type="number" placeholder="cost per item?"/>
+    //       <button onClick={addItem} className="bg-orange-800 hover:bg-orange-600 p-3 text-xl rounded-lg text-white" type="submit">add</button>
+    //     </form>
 
-//         <h1 className="text-3xl p-3 text-center">current items...</h1>
-//         <ul>
-//           {items.map((item, id) => (
-//             <li key={id} className="my-4 w-full flex justify-between mx-2 bg-orange-800 rounded-lg">
-//               <div className="p-3 w-full flex justify-between mx-3"> 
-//                 <span className="lowercase">{item.name}</span>
-//                 <span>x{item.amt}</span>
-//                 <span>${item.cost}</span>
-//               </div>
-//               <button onClick={() => deleteItem(item.id)} className="ml-8 p-3 border-orange-900 hover:bg-orange-600 w-10 rounded-lg">x</button>
-//             </li>
-//           ))}
-//         </ul>
+    //     <h1 className="text-3xl p-3 text-center">current items...</h1>
+    //     <ul>
+    //       {items.map((item, id) => (
+    //         <li key={id} className="my-4 w-full flex justify-between mx-2 bg-orange-800 rounded-lg">
+    //           <div className="p-3 w-full flex justify-between mx-3"> 
+    //             <span className="lowercase">{item.name}</span>
+    //             <span>x{item.amt}</span>
+    //             <span>${item.cost}</span>
+    //           </div>
+    //           <button onClick={() => deleteItem(item.id)} className="ml-8 p-3 border-orange-900 hover:bg-orange-600 w-10 rounded-lg">x</button>
+    //         </li>
+    //       ))}
+    //     </ul>
 
-//         {items.length < 1 ? ('there are no items in your bag...') : ( 
-//           <div className="flex justify-between p-3">
-//             <span>total :</span> <span>${total}</span>
-//           </div>
-//         )}
+    //     {items.length < 1 ? ('there are no items in your bag...') : ( 
+    //       <div className="flex justify-between p-3">
+    //         <span>total :</span> <span>${total}</span>
+    //       </div>
+    //     )}
 
-//       </div>
+    //   </div>
 
-//       </div>
-//     </main>
+    //   </div>
+    // </main>
 //   );
 // }
